9886781eab09ea91cb706c70315aad6048eb166e
Mohit kumawat <mohitkumawat221004@gmail.com>
2025-09-15 22:01:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 require('dotenv').config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 const urlRoutes = require('./routes/url');
 app.use('/', urlRoutes);
 
